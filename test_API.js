e1fecc663dfe09a5ef517e634368076fad75b7ae
roblee357 <roblee357@hotmail.com>
2020-10-08 13:42:52 -0400

    Added project.js to test_API.js

--- test_API.js
+++ test_API.js
@@ -1,4 +1,38 @@
 
+
+var url = "https://clever-beaver-3f5cd2.netlify.app/data.json";
+var table;
+$(document).ready(function() {
+  $.ajax({
+    url: url,
+    success: function(data){
+      console.log(data.Jobs[0])
+      table = $('#table').dataTable({
+        data: data.Jobs,
+        columns: [{
+          data: 'JvId',
+          title: 'Job ID'
+        },{
+          data: 'JobTitle',
+          title: 'Job Title'
+        },{
+          data: 'Company',
+          title: 'Company'
+        },{
+          data: 'AccquisitionDate',
+          title: 'Accquisition Date'
+        },{
+          data: 'URL',
+          title: 'URL'
+        },{
+          data: 'Location',
+          title: 'Location'
+        }]
+      });
+    }
+  })
+});
+
 function fetch_Occupations(jobName ,location ) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
