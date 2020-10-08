988b56e2d865b9ff94327b6248d7891053da1ae9
roblee357 <roblee357@hotmail.com>
2020-10-08 15:22:15 -0400

    destroying table

--- test_API.js
+++ test_API.js
@@ -112,7 +112,8 @@ function fetch_Occupations(jobName ,location ) {
 function fetch_jobs(jobName ,location ) {
 var table = $('#myTable').DataTable();
   table.destroy();
-  document.getElementById("table").outerHTML = "";
+  tbl = document.getElementById("table");
+  if(tbl) tbl.parentNode.removeChild(tbl);
   console.log(table.innerHTML)
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
